b6c9db280d9aa29413f6b1af0a17f6ba6bd9df5d
Gustavo Homem <gustavo.homem@wiseathena.com>
2016-02-22 00:13:02 +0000

    clarify security on logout.php

--- WPCute/customer_folder_template/logout.php
+++ WPCute/customer_folder_template/logout.php
@@ -2,11 +2,32 @@
 
 require_once("../config.php");
 
-// we must not be called directly
-session_start();
-if ( !isset($_SESSION['userid'] ) ) die("Don't go there Dave");
+// if we have a session we clean it up first - that is the main function here
+// otherwise, ie, if we were called directly for some reason, we just redirect
 
-session_destroy();
+// of course the friendly redirect on direct invocation of this page confirms
+// user existence; we could do 404 instead but an accidental double-click on
+// the logout link would bring up and error page - and that happens when people
+// user a "file explorer"
+
+session_start();
+
+if ( isset($_SESSION['userid'] ) ) 
+{
+  session_destroy();
+}
+else
+{
+
+  // place holder for avoiding user existence disclosure at the cost less friendliness
+
+  /* $mymsg = str_replace("{{ DUMMYURL }}", $_SERVER['REQUEST_URI'], $MSG404);
+  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+  print $mymsg;
+  die( ); */ 
+
+}
+
 header("Location: ".$REDIRURL);
 
 ?>
